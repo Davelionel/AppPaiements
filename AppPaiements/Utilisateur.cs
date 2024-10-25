21a6648cd0547dd30af76d35eb9cdb4d8b0450b4
dave lionel <300135134@monboreal.ca>
2024-10-25 16:15:47 -0400

    ajout de la classe Utilisateur

---
+++ AppPaiements/Utilisateur.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppPaiements
+{
+    internal class Utilisateur
+    {
+        public string Nom { get; set; }
+        public List<Paiement> Paiements { get; set; }
+        public Utilisateur(string nom)
+        {
+            Nom = nom;
+            Paiements = new List<Paiement>();
+        }
+        public void AfficherInfos()
+        {
+            Console.WriteLine($"Nom: {Nom}");
+            foreach (var paiement in Paiements)
+            {
+                paiement.AfficherDetails();
+            }
+        }
+
+        public void AjouterPaiement(Paiement paiement)
+        {
+            Paiements.Add(paiement);
+        }
+    }
+}
