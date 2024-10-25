de7bda68fa7d185b4dcb8195b029591d94fce88a
dave lionel <300135134@monboreal.ca>
2024-10-25 13:15:22 -0400

    Ajout de la classe fille Paypal

---
+++ AppPaiements/Paypal.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppPaiements
+{
+    internal class Paypal : Paiement
+    {
+        public string Courriel { get; set; }
+        public Paypal(double montant, string description, string email) : base(montant, description)
+        {
+            Courriel = email;
+        }
+        public override void AfficherDetails()
+        {
+            Console.WriteLine($"Montant: {Montant}, Description: {Description}, Courriel: {Courriel}");
+        }
+    }
+}
--- AppPaiements/Program.cs
+++ AppPaiements/Program.cs
@@ -4,7 +4,7 @@ internal class Program
 {
     public static void Main(string[] args)
     {
-        
+
         CarteCredit achat1 = new CarteCredit(100.50, "epicerie", 1232545154);
         CarteCredit achat2 = new CarteCredit(200.75, "shopping", 234567890);
         CarteCredit achat3 = new CarteCredit(50.25, "medicament", 345678901);
@@ -12,6 +12,14 @@ internal class Program
         achat1.AfficherDetails();
         achat2.AfficherDetails();
         achat3.AfficherDetails();
-        
+
+    
+    
+        Paypal paiement1 = new Paypal(1150, "iphone 16 pro", "fac32@gmail.com");
+        Paypal paiement2 = new Paypal(75.50, "chemise", "davlio32@gmail.com");
+
+        paiement1.AfficherDetails();
+        paiement2.AfficherDetails();
+
     }
 }
